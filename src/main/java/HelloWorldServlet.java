66352da903eb1231f58ae1bda5e5f1d6d35a7d11
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-10-18 14:20:56 -0500

    finish exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,44 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+
+
+
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    private int hitCount;
+    public void init() {
+        // Reset hit counter.
+        hitCount = 0;
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        String name = req.getParameter("name");
+        String count = req.getParameter("count");
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        if (name == null) {
+            out.println("<h1>Hello, World!</h1>");
+        }else {
+            out.println("<h1>Hello, " + name + "!");
+        }
+        if(count.equalsIgnoreCase( "reset")){
+            init();
+            out.println("<p>total page visits: " + hitCount + " </p>");
+        }else {
+            hitCount++;
+
+            out.println("<p>total page visits: " + hitCount + " </p>");
+        }
 
     }
+
+
 }
